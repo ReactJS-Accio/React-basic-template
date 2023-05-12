8f17f4c32e8b04e07ad59cb1a3d218bc2c363373
thomsontk97 <thomsontk237@gmail.com>
2023-05-12 11:36:25 +0530

    gh-pages added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,29 @@ import Navbar from "./Components/Navbar";
 import About from "./Components/About";
 import Section from "./Components/Section";
 
+let content = [
+  {
+    title: "Skills",
+    content:
+      "Lorem Lorem ipsum dolor sit amet,Lorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elit  consectetur adipisicing elit Lorem Lorem ipsum dolor sit amet, consectetur adipisicing elit  Lorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elit",
+  },
+  {
+    title: "Experience",
+    content:
+      "Lorem Lorem ipsum dolor sit amet, Lorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elit consectetur adipisicing elit. Lorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elit",
+  },
+  {
+    title: "Education",
+    content:
+      "Lorem Lorem ipsum dolor sit amet, Lorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elit consectetur adipisicing elit Lorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elit",
+  },
+  {
+    title: "Projects",
+    content:
+      "Lorem Lorem ipsum dolor sit amet, . Lorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elit.consectetur adipisicing elit Lorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem Lorem ipsum dolor sit amet, consectetur adipisicing elit",
+  },
+];
+
 function App() {
   return (
     <div>
@@ -10,7 +33,13 @@ function App() {
       <About>
         <hr />
       </About>
-      <Section title="Skills" content="sdffjlsjflsjfljslfjslfjlsjfl" />
+      {content.map((item, index) => {
+        return (
+          <Section key={index} title={item.title} content={item.content}>
+            {index !== content.length - 1 ? <hr /> : ""}
+          </Section>
+        );
+      })}
     </div>
   );
 }
--- src/Components/Section.js
+++ src/Components/Section.js
@@ -4,6 +4,7 @@ const Section = ({ title, content, children }) => (
   <div className="section">
     <h1>{title}</h1>
     <p>{content}</p>
+    {children}
   </div>
 );
 
